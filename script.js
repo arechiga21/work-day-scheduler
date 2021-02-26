d7833654192189a6f9526080140e6b57052bc593
arechiga2 <arechiga21jj@gmail.com>
2021-02-26 03:33:37 -0700

    added color change depending on time

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 var currentDay = moment().format('dddd MMMM Do');
 var momentId = document.getElementById('currentDay');
 momentId.innerHTML = currentDay;
+
+function timeBlockColor(){
+    $(".time-block").each(function(){
+
+      if (parseInt($(this).attr("data-hour")) == moment().format("H")) {
+        $(this).addClass("present").removeClass("past future");
+      }
+      if (parseInt($(this).attr("data-hour")) < moment().format("H")) {
+        $(this).addClass("past").removeClass("present future");
+      }
+      if (parseInt($(this).attr("data-hour")) > moment().format("H")) {
+        $(this).addClass("future").removeClass("past present");
+      }
+    });
+}
+
+timeBlockColor()
